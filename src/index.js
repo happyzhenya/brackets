9880de8ba05fd1f458a7028d37ff6a3229ddb1f4
happyzhenya <happy.zhenya@gmail.com>
2022-01-31 00:39:05 +0300

    fix: change

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  if (bracketsConfig % 2===1) {
-    return false;
-  }
-  const openBraskets=[];
-  for (let i = 0; i <str.lenght; i ++){
-     
-     let brasketsPairIndex=bracketsConfig.findIndex(el=>el.incudes(str[i]));
-     if (-1===brasketsPairIndex) {break};
-     if (0===bracketsConfig[brasketsPairIndex].indexOf(srt[i])){
-      openBraskets.push(brasketsPairIndex)
-     } else{
-       if (openBraskets.pop()!==brasketsPairIndex){return false}
-     }
+  if ( str.length % 2 == 1 ) {return false}
 
+  for ( let i = 0; i < bracketsConfig.length; i++ ) {
+    let bracketsPairs = bracketsConfig[i].join('')
+
+    if ( str.includes(bracketsPairs) ) {
+      str = str.replace(bracketsPairs, '')
+      i = -1
+    }
   }
-  return 0===openBraskets.lenght;
+  return str.length === 0
 }
