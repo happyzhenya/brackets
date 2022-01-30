5ecb816b6595e58b6c76f9a6f34a973647179f3c
happyzhenya <happy.zhenya@gmail.com>
2022-01-30 23:44:45 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (bracketsConfig % 2===1) {
+    return false;
+  }
+   
+  for (let i = 0; i <str.lenght; i ++){
+      const openBaskets=[]
+     let basketsPairIndex=bracketsConfig.findIndex(el=>el.incudes(str[i]));
+     if (-1===basketsPairIndex) {break};
+     if (0===bracketsConfig[basketsPairIndex].indexOf(srt[i])){
+      openBaskets.push(basketsPairIndex)
+     } else{
+       if (openBaskets.pop()!==basketsPairIndex){return false}
+     }
+
+  }
+  return 0===openBaskets.lenght;
 }
